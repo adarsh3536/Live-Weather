c9d479f2c224b93320422b80fd7e067b58bfa615
Adarsh <adarshgupta7103@gmail.com>
2024-05-27 14:23:31 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -3,29 +3,14 @@ import "./App.css";
 
 const App = () => {
   const [city, setCity] = useState(null);
-  const [search, setSearch] = useState("Pune");
-
-  // useEffect(() => {
-  //   fetch(
-  //     `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e114695862036dc3dbf3a28633955d60`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setInput(data);
-  //       setSearch(response);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching data using fetch API:", error);
-  //     });
-  // }, []);
+  const [search, setSearch] = useState("Mumbai");
 
   useEffect(() => {
     const fetchApi = async () => {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=e114695862036dc3dbf3a28633955d60`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=e114695862036dc3dbf3a28633955d60`;
       const res = await fetch(url);
-      const resJson = res.json();
-      console.log(resJson);
-      setCity(resJson);
+      const resJson = await res.json();
+      setCity(resJson.main);
     };
 
     fetchApi();
@@ -35,21 +20,38 @@ const App = () => {
     <div className="full_page">
       <div className="main">
         <input
+          value={search}
           type="search"
           onChange={(event) => {
             setSearch(event.target.value);
           }}
         />
-        <h2>
-          <i className="fa-solid fa-street-view"></i> {search}
-        </h2>
-        <b>34</b>
-        <p>Min: 33.24°C | Max: 29.24°C</p>
-        <section className="section">
-          <div className="wave wave1"></div>
-          <div className="wave wave2"></div>
-          <div className="wave wave3"></div>
-        </section>
+
+        {!city ? (
+          <>
+            <p className="noDataFound">No Data Found</p>
+            <section className="section">
+              <div className="wave wave1"></div>
+              <div className="wave wave2"></div>
+              <div className="wave wave3"></div>
+            </section>
+          </>
+        ) : (
+          <>
+            <h2>
+              <i className="fa-solid fa-street-view"></i> {search}
+            </h2>
+            <b>{city.temp}°C</b>
+            <p>
+              Min: {city.temp_min}°C | {city.temp_max}°C
+            </p>
+            <section className="section">
+              <div className="wave wave1"></div>
+              <div className="wave wave2"></div>
+              <div className="wave wave3"></div>
+            </section>
+          </>
+        )}
       </div>
     </div>
   );
